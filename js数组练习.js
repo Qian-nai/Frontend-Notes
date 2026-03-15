0cf2a5f6c1ff1c6fe2bfe6bb4d0e6b343d8da818
qiaNai <2452722313@qq.com>
2026-03-15 21:37:25 +0800

    3.15

--- js数组练习.js
+++ js数组练习.js
@@ -51,3 +51,83 @@ function reverse(array) {
     }
     return newArr;
 }
+
+//【重要】方法的封装：每间隔30ms，将盒子向右移动10px
+    function animate(ele, target) {
+        //要用定时器，先清除定时器
+        //一个盒子只能有一个定时器，这样的话，不会和其他盒子出现定时器冲突
+        //我们可以把定时器本身，当成为盒子的一个属性
+        clearInterval(ele.timer);
+        //我们要求盒子既能向前又能向后，那么我们的步长就得有正有负
+        //目标值如果大于当前值取正，目标值如果小于当前值取负
+        var speed = target > ele.offsetLeft ? 10 : -10;  //speed指的是步长
+        ele.timer = setInterval(function () {
+            //在执行之前就获取当前值和目标值之差
+            var val = target - ele.offsetLeft;
+
+            //移动的过程中，如果目标值和当前值之差如果小于步长，那么就不能在前进了
+            //因为步长有正有负，所有转换成绝对值来比较
+            if (Math.abs(val) < Math.abs(speed)) {  //如果val小于步长，则直接到达目的地；否则，每次移动一个步长
+                ele.style.left = target + "px";
+                clearInterval(ele.timer);
+            } else {
+                ele.style.left = ele.offsetLeft + speed + "px";
+            }
+        }, 30)
+    }
+
+
+    //promise练习
+    let promise = new Promise((resolve, reject) =>{
+        resolve("成功了");
+        //reject("失败了");
+    })
+    promise.then(data =>{
+        console.log(data);
+
+        return new Promise((resolve, reject) =>{
+            resolve("又成功了");
+            //reject("又失败了");
+        })
+    }, err =>{
+        console.log(err);
+        throw new Error("又失败了");
+    })
+
+    .then(data =>{
+        console.log(data);
+    }, err =>{
+        console.log(err);
+    })
+
+    .catch(err =>{
+        console.log(err);
+    })
+
+    //async/await练习
+    //创建一个函数，返回一个Promise对象
+    function asyncTask() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("异步任务完成");
+                //reject("异步任务失败");
+            }, 1000);
+            const isSuccess = true; //模拟异步任务的成功或失败
+            if (isSuccess) {
+                resolve("异步任务完成");
+            } else {
+                reject("异步任务失败");
+            }
+        });
+    }
+    //使用async/await调用异步函数
+    async function main() {
+        try {
+            const result = await asyncTask();
+            console.log(result); // 输出: 异步任务完成
+        } catch (error) {
+            console.error(error); // 输出: 异步任务失败
+        }
+    }
+
+    main();
